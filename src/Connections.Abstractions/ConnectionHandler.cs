ac6170f269448b94e9e58fa7a34404c763fe1ff3
David Fowler <davidfowl@gmail.com>
2018-03-24 04:42:38 -0700

    Changed comment to unblock signalr merge to dev

--- src/Connections.Abstractions/ConnectionHandler.cs
+++ src/Connections.Abstractions/ConnectionHandler.cs
@@ -5,13 +5,10 @@ using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Connections
 {
-    /// <summary>
-    /// Represents an end point that multiple connections connect to. For HTTP, endpoints are URLs, for non HTTP it can be a TCP listener (or similar)
-    /// </summary>
     public abstract class ConnectionHandler
     {
         /// <summary>
-        /// Called when a new connection is accepted to the endpoint
+        /// Called when a new connection connects
         /// </summary>
         /// <param name="connection">The new <see cref="ConnectionContext"/></param>
         /// <returns>A <see cref="Task"/> that represents the connection lifetime. When the task completes, the connection is complete.</returns>
